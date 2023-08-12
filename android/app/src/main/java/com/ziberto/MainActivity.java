73a44b7b50812d73a847f1a51f8895d1903aaebe
Rajput-surendra <surendraalphawizz@gmail.com>
2023-08-12 15:11:09 +0530

    12 Aug

---
+++ android/app/src/main/java/com/ziberto/MainActivity.java
@@ -0,0 +1,6 @@
+package com.lifegracious.users;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,22 @@
+final String appName = 'DELI';
+final String packageName = 'com.lifegracious.users';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'com.lifegracious.users';
+final String iosLink = 'your ios link here';
+final String appStoreId = '123456789';
+
+final String deepLinkUrlPrefix = 'https://eshopmultivendor.page.link';
+final String deepLinkName = 'eshop';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+//final String baseUrl = 'https://vendor.eshopweb.store/app/v1/api/';
+// final String baseUrl = 'https://alphawizztest.tk/Maangos_Ecom/app/v1/api/';
+final String baseUrl = 'https://lifegracious.com/app-admin/app/v1/api/';
+// final String imageUrl = 'https://alphawizztest.tk/Maangos_Ecom/';
+final String imageUrl = 'https://lifegracious.com/app-admin';
+
+
+final String jwtKey = "78084f1698c9fcff5a668b68dcd103db39be2605";
